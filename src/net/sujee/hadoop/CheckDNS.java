b529d4afd75940ca845df3558f4af684e2d0f04c
Sujee Maniyam <sujee.maniyam@hds.com>
2012-02-15 12:40:43 -0800

    self check, hosts file optional
    - self check: checks the local host name as well
    
    - hosts file optional : if no hosts file is given, just does self
      checking

--- src/net/sujee/hadoop/CheckDNS.java
+++ src/net/sujee/hadoop/CheckDNS.java
@@ -12,90 +12,91 @@ import java.util.List;
  * check dns and reverse dns of hosts in a given file
  * 
  * @author sujee@sujee.net
- *
+ * 
  */
-public class CheckDNS
-{
+public class CheckDNS {
 
-    public static void main(String[] args) throws Exception
-    {
-        if (args.length < 1)
-        {
-            System.out.println ("needs hosts file(s)");
-            System.exit(1);
-        }
-        
-        InetAddress localhost = InetAddress.getLocalHost();
-        System.out.println ("==== Running on : " + localhost + " =====");
-        for (String s : args)
-        {
-            String[] hosts = readLinesFromFile(s, true);
-            for (String host : hosts)
-            {
-                System.out.println ("-- host : " + host);
-                
-                InetAddress inet = null;
-                try
-                {
-                    inet = InetAddress.getByName(host);
-                    String ip = inet.getHostAddress();
-                    System.out.println ("   host lookup : success (" + ip + ")");
-                } catch (UnknownHostException e)
-                {
-                    System.out.println ("   host lookup : *** failed ***" );
-                    e.printStackTrace();
-                }
-                
-                // reverse
-                if (inet != null)
-                {
-                    try
-                    {
-                        InetAddress revHost = InetAddress.getByAddress(inet.getAddress());
-                        String revHostName = revHost.getHostName();
-                        if (host.equals(revHostName))
-                            System.out.println ("   reverse lookup : success (" + revHostName + ")");
-                        else
-                            System.out.println ("   reverse lookup : ***failed*** (" + revHostName + " != " + host + " (expected) )");
-                    }
-                    catch (UnknownHostException e)
-                    {
-                        System.out.println ("   reverse lookup : ***failed***" );
-                        
-                    }
-                    
-                    if (inet.isReachable(10))
-                        System.out.println ("   is reachable : yes");
-                    else
-                        System.out.println ("   is reachable : *** no ***");
-                    
-                }
-                System.out.println ("");
-            } // end for loop on hosts
-        }
-    }
-    
-    public static String [] readLinesFromFile(String fileName, boolean skipCommentsAndBlanks) throws IOException
-    {
-        List<String> lines = new ArrayList<String>();
-        BufferedReader reader = new BufferedReader(new FileReader(fileName));
-        
-        String line = null;
-        while ( (line = reader.readLine()) != null)
-        {
-            if (skipCommentsAndBlanks)
-            {
-                line = line.trim();
-                if (line.length() == 0 ) continue;
-                
-                if (line.startsWith("#")) continue;
-            }
-            lines.add(line);
-        }
-        
-        String [] arrLines = new String[lines.size()];
-        lines.toArray(arrLines);
-        return arrLines;
-    }
+	public static void main(String[] args) throws Exception {
+
+		InetAddress localhost = InetAddress.getLocalHost();
+		System.out.println("==== Running on : " + localhost + " =====");
+		System.out.println("# self check...");
+		checkHost(localhost.getHostName());
+		System.out.println("# end self check\n");
+		
+		for (String s : args) {
+			String[] hosts = readLinesFromFile(s, true);
+			for (String host : hosts) {
+				checkHost(host);
+				System.out.println("");
+			} 
+		}
+	}
+
+	public static void checkHost(String host) {
+		System.out.println("-- host : " + host);
+
+		InetAddress inet = null;
+		try {
+			inet = InetAddress.getByName(host);
+			String ip = inet.getHostAddress();
+			System.out.println("   host lookup : success (" + ip + ")");
+		} catch (UnknownHostException e) {
+			System.out.println("   host lookup : *** failed ***");
+			e.printStackTrace();
+		}
+
+		// reverse
+		if (inet != null) {
+			try {
+				InetAddress revHost = InetAddress.getByAddress(inet
+						.getAddress());
+				String revHostName = revHost.getHostName();
+				if (host.equals(revHostName))
+					System.out.println("   reverse lookup : success ("
+							+ revHostName + ")");
+				else
+					System.out.println("   reverse lookup : ***failed*** ("
+							+ revHostName + " != " + host + " (expected) )");
+			} catch (UnknownHostException e) {
+				System.out.println("   reverse lookup : ***failed***");
+
+			}
+			try {
+				if (inet.isReachable(10))
+					System.out.println("   is reachable : yes");
+				else
+					System.out.println("   is reachable : *** failed ***");
+			} catch (IOException e) {
+				System.out.println("   is reachable  : *** failed ***");
+				e.printStackTrace();
+			}
+
+		}
+		
+	}
+
+	public static String[] readLinesFromFile(String fileName,
+			boolean skipCommentsAndBlanks) throws IOException {
+		List<String> lines = new ArrayList<String>();
+		BufferedReader reader = new BufferedReader(new FileReader(fileName));
+
+		String line = null;
+		while ((line = reader.readLine()) != null) {
+			if (skipCommentsAndBlanks) {
+				line = line.trim();
+				if (line.length() == 0)
+					continue;
+
+				if (line.startsWith("#"))
+					continue;
+			}
+			lines.add(line);
+		}
+
+		String[] arrLines = new String[lines.size()];
+		lines.toArray(arrLines);
+		return arrLines;
+	}
 
 }
